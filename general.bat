1dcc86ee91e441c903066994b2839fda6ed68fb7
HolyLight_ <46376457544a@gmail.com>
2026-03-15 22:41:07 +0700

    1.4.3

--- general.bat
+++ general.bat
@@ -9,7 +9,7 @@ set "LISTS=%~dp0exp-list\"
 set "GameFilter=1024-65535"
 
 @echo off
-powershell -Command "Write-Host 'Самые быстрые фиксы беспредела РКН' -ForegroundColor DarkMagenta; Write-Host 'Расчитано и проверяется в Сибирском регионе' -ForegroundColor Magenta; Write-Host 'Мои соц. сетки:' -ForegroundColor White; Write-Host 'https://t.me/notholylab' -ForegroundColor Cyan; Write-Host 'https://github.com/HolyLightRU' -ForegroundColor White; Write-Host 'Так-же в папках additional и flowseal_strat' -ForegroundColor Magenta; Write-Host 'Лежат дополнительные стратегии' -ForegroundColor DarkMagenta"
+powershell -Command "Write-Host 'Самые быстрые фиксы беспредела РКН' -ForegroundColor DarkMagenta; Write-Host 'Расчитано и проверяется в Сибирском регионе' -ForegroundColor Magenta; Write-Host 'Мои соц. сетки:' -ForegroundColor White; Write-Host 'https://github.com/HolyLightRU' -ForegroundColor White; Write-Host 'Так-же в папках additional и flowseal_strat' -ForegroundColor Magenta; Write-Host 'Лежат дополнительные стратегии' -ForegroundColor DarkMagenta"
 
 "%BIN%HolyZapret.exe" --wf-tcp=80,443,2053,2083,2087,2096,8443,%GameFilter% --wf-udp=443,1400,19294-19344,50000-50100,%GameFilter% ^
 --filter-udp=443 --hostlist="%LISTS%list-general.txt" --hostlist-exclude="%LISTS%list-exclude.txt" --ipset-exclude="%LISTS%ipset-exclude.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
